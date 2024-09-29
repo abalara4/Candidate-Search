415313f0219cdd977dc90a747b57245031baf2b8
abalara4 <abalara4@gmail.com>
2024-09-29 14:15:54 -0400

    Candidate Interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    resume: string;
+    job: string;
+    status: string;
+    created_at: string;
+    updated_at: string;
+}
